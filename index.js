eb53bd19994f80ef076273614b9a7b60bafe703a
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-04-26 14:15:06 +0530

    Update flie

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
+require("dotenv").config();
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/", (req, res) => {
+  if (process.env.IS_ADMIN === "true") {
+    res.send({
+      message: "Welcome, Admin!",
+      data: ["Admin Data 1", "Admin Data 2"],
+    });
+  } else {
+    res.send({
+      message: "Welcome, User!",
+      data: ["User Data 1", "User Data 2"],
+    });
+  }
 });
 
 app.listen(port, () => {
